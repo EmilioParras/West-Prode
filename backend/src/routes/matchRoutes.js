0c7f68f364ed9b8927e78710d29510853771fd6a
EmilioParras <parrasemilio@gmail.com>
2025-12-18 22:25:33 -0300

    Agregue rutaa para consultar mi DB.

--- backend/src/routes/matchRoutes.js
+++ backend/src/routes/matchRoutes.js
@@ -1,11 +1,32 @@
 const express = require('express');
-const router = express.Router();    
+const router = express.Router(); 
+const prisma = require('../config/db');
 const { getGamesCompetition } = require('../services/footbalService');
 
-router.get('/:code', async (req, res) => {
-    const { code } = req.params; // Ejemplo: 'PL' o 'PD'
-    const result = await getGamesCompetition(code.toUpperCase()); // Espera que getGamesCompetiton devuelva los partidos de esa competicion.
+// 1. RUTA DE SINCRONIZACIÓN (La que va a la API externa)
+router.get('/sync/:code', async (req, res) => {
+    const { code } = req.params;
+    const result = await getGamesCompetition(code.toUpperCase());
     res.json(result);
 });
 
-module.exports = router;  
+// 2. RUTA DE LECTURA CON FILTRO (La que va a TU base de datos)
+// URL: localhost:3000/partidos  (Trae todo)
+// URL: localhost:3000/partidos?liga=Premier League (Trae solo Premier)
+router.get('/', async (req, res) => {
+    try {
+        const { liga } = req.query; 
+        
+        const matches = await prisma.match.findMany({
+            where: liga ? { leagueName: liga } : {}, 
+            orderBy: { matchDate: 'asc' }
+        });
+        
+        res.json(matches);
+    } catch (error) {
+        console.error("❌ Error al obtener partidos:", error.message);
+        res.status(500).json({ error: "Error al obtener partidos" });
+    }
+});
+
+module.exports = router;
