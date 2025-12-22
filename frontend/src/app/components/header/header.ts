379a97a80f672348ab7e7c0aa960176351243ca4
EmilioParras <parrasemilio@gmail.com>
2025-12-22 00:06:13 -0300

    Correcciones en el header, nombre de un archivo. Instale FontAwesome para usos de iconos.

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; 
+import { faBars } from '@fortawesome/free-solid-svg-icons'; // Icono de hamburguesa
+import { faUser } from '@fortawesome/free-solid-svg-icons'; // Icono de perfil de usuario
 
 @Component({
-  selector: 'app-header',
-  imports: [RouterModule],
+  selector: 'header-component',
+  standalone: true,
+  imports: [RouterModule, CommonModule, FontAwesomeModule], 
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.css'
 })
+
 export class HeaderComponent {
+  iconoHamburguesa = faBars;
+  iconoUserProfile = faUser; 
+
+  isMenuOpen = false;
+  isUserProfileOpen = false;
+  toggleHamburguer() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleUserProfile() {
+    this.isUserProfileOpen = !this.isUserProfileOpen;
+  }
 
 }
